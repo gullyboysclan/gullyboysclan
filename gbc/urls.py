c1d43d8584a6c862c081c2f4213129ba1ad10dce
Kwaku Appenteng Wiredu <kwakuwiredu0@gmail.com>
2024-11-10 19:05:26 -0800

    upgrades to vercel configuraiton

--- gbc/urls.py
+++ gbc/urls.py
@@ -23,6 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('store.urls'))
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
